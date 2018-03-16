24ff9dd9cb88f5c5480e80146131bc5288d39b8f
yujiakimoto <ya242@cornell.edu>
2018-03-16 13:11:58 -0400

    Updated RNN model

---
+++ 04-Recurrent-Neural-Networks/rnn_model.py
@@ -0,0 +1,105 @@
+"""
+TensorFlow implementation of a character-level RNN.
+Adapted from https://github.com/sherjilozair/char-rnn-tensorflow/blob/master/model.py
+"""
+
+import numpy as np
+import tensorflow as tf
+from tensorflow.contrib import rnn
+from tensorflow.contrib import legacy_seq2seq
+
+
+class RNN:
+    def __init__(self, args, training=True):
+        """Initialize RNN model"""
+        self.args = args
+        if not training:
+            args.batch_size = 1
+            args.seq_length = 1
+
+        cell_fn = rnn.GRUcell
+        cells = []
+        for _ in range(args.num_layers):
+            cell = cell_fn(args.rnn_size)
+            cells.append(cell)
+
+        self.cell = rnn.MultiRNNCell(cells, state_is_tuple=True)
+        self.input_data = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
+        self.targets = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
+        self.initial_state = cell.zero_state(args.batch_size, tf.float32)
+
+        with tf.variable_scope('rnn'):
+            softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
+            softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
+
+        embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
+        inputs = tf.nn.embedding_lookup(embedding, self.input_data)
+
+        inputs = tf.split(inputs, args.seq_length, 1)
+        inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+
+        def loop(prev, _):
+            prev = tf.matmul(prev, softmax_w) + softmax_b
+            prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
+            return tf.nn.embedding_lookup(embedding, prev_symbol)
+
+        outputs, last_state = legacy_seq2seq.rnn_decoder(inputs, self.initial_state, cell,
+                                                         loop_function=loop if not training else None, scope='rnnlm')
+        output = tf.reshape(tf.concat(outputs, 1), [-1, args.rnn_size])
+
+        self.logits = tf.nn.xw_plus_b(output, softmax_w, softmax_b)
+        self.probs = tf.nn.softmax(self.logits)
+        loss = legacy_seq2seq.sequence_loss_by_example(
+                [self.logits],
+                [tf.reshape(self.targets, [-1])],
+                [tf.ones([args.batch_size * args.seq_length])])
+
+        with tf.name_scope('loss'):
+            self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
+
+        self.final_state = last_state
+        self.learning_rate = tf.Variable(0.0, trainable=False)
+        tvars = tf.trainable_variables()
+        grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars), args.grad_clip)
+
+        with tf.name_scope('optimization'):
+            optimizer = tf.train.AdamOptimizer(self.learning_rate)
+        self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+
+    def sample(self, sess, chars, vocab, num=200, prompt='The '):
+        """Generate predictions from RNN.
+
+        Args:
+            sess (tf.Session): a tensorflow session containing the RNN graph from which to generate predictions
+            chars (dict):      a dictionary mapping indices to characters (e.g. {1: 'a', 2: 'b'})
+            vocab (dict):      a dictionary mapping characters to indices; inverse of chars (e.g. {'a': 1, 'b': 2})
+            num (int):         length of string to generate (not including prompt)
+            prompt (str):      prompt to start generation (i.e. beginning of predicted text)
+
+        Returns:
+            str:               RNN-generated text
+        """
+        state = sess.run(self.cell.zero_state(1, tf.float32))
+        for char in prompt[:-1]:
+            x = np.zeros((1, 1))
+            x[0, 0] = vocab[char]
+            feed = {self.input_data: x, self.initial_state: state}
+            [state] = sess.run([self.final_state], feed)
+
+        def weighted_pick(weights):
+            t = np.cumsum(weights)
+            s = np.sum(weights)
+            return int(np.searchsorted(t, np.random.rand(1) * s))
+
+        output = prompt
+        char = prompt[-1]
+        for n in range(num):
+            x = np.zeros((1, 1))
+            x[0, 0] = vocab[char]
+            [probs, state] = sess.run([self.probs, self.final_state], {self.input_data: x, self.initial_state: state})
+            p = probs[0]
+            sample = weighted_pick(p)
+            pred = chars[sample]
+            output += pred
+            char = pred
+        return output
--- 04-Recurrent-Neural-Networks/rnn_train.py
+++ 04-Recurrent-Neural-Networks/rnn_train.py
@@ -1,101 +1,31 @@
 """
-Train a character-level RNN.
+Train the character-level RNN described in rnn_model.py
 """
 
-import numpy as np
+import argparse
 import tensorflow as tf
-import time
-import sys
-from tensorflow.contrib.rnn import BasicLSTMCell, MultiRNNCell, static_rnn
 
 
-train_file = sys.argv[1]
-with open(train_file, 'r') as f:
-    transcript = f.read()
-transcript = transcript.replace('\n', ' ')
-unique_chars = sorted(list(set(transcript)))
-num_classes = len(unique_chars)
-char2idx = dict(zip(unique_chars, range(num_classes)))
+def train(args):
+    pass
 
-# hyperparameters
-num_steps = 50
-num_layers = 2
-state_size = 128
-batch_size = 50
-learning_rate = 0.001
-max_epochs = 50
 
-# input & embedding layer
-x = tf.placeholder(tf.int32, [batch_size, num_steps])
-y = tf.placeholder(tf.int32, [batch_size, num_steps])
-one_hot_embeddings = tf.eye(num_classes)
-x_embed = tf.nn.embedding_lookup(one_hot_embeddings, x)
-y_embed = tf.nn.embedding_lookup(one_hot_embeddings, y)
-x_inputs = tf.unstack(x_embed, axis=1)
-y_outputs = tf.unstack(y_embed, axis=1)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', type=str, default='../data/', help='data directory containing input.txt')
+    parser.add_argument('--save_dir', type=str, default='../models/rnn/', help='directory to store checkpointed models')
+    parser.add_argument('--rnn_size', type=int, default=128, help='size of RNN hidden state')
+    parser.add_argument('--num_layers', type=int, default=2, help='number of layers in the RNN')
+    parser.add_argument('--batch_size', type=int, default=50, help='minibatch size')
+    parser.add_argument('--seq_length', type=int, default=50, help='RNN sequence length')
+    parser.add_argument('--num_epochs', type=int, default=50, help='number of epochs')
+    parser.add_argument('--save_every', type=int, default=1000, help='save frequency')
+    parser.add_argument('--grad_clip', type=float, default=5., help='clip gradients at this value')
+    parser.add_argument('--learning_rate', type=float, default=0.002, help='learning rate')
+    parser.add_argument('--decay_rate', type=float, default=0.97, help='decay rate for rmsprop')
+    args = parser.parse_args()
+    train(args)
 
-# RNN cells
-cells = [BasicLSTMCell(state_size) for _ in range(num_layers)]
-cell = MultiRNNCell(cells)
-init_state = cell.zero_state(batch_size, tf.float32)
-rnn_outputs, _ = static_rnn(cell, x_inputs, initial_state=init_state)
 
-# softmax layer
-with tf.variable_scope('softmax'):
-    W = tf.get_variable('W', [state_size, num_classes])
-    b = tf.get_variable('b', [num_classes], initializer=tf.constant_initializer(0.0))
-logits = [tf.matmul(rnn_output, W) + b for rnn_output in rnn_outputs]
-preds = [tf.nn.softmax(logit) for logit in logits]
-
-losses = [tf.nn.softmax_cross_entropy_with_logits_v2(labels=label, logits=logit)
-          for label, logit in zip(y_outputs, logits)]
-
-total_loss = tf.reduce_mean(losses)
-train_step = tf.train.AdamOptimizer(learning_rate=0.001).minimize(total_loss)
-correct = [tf.cast(tf.equal(tf.argmax(pred, 1), tf.argmax(y_output, 1)), tf.float32) for
-           pred, y_output in zip(preds, y_outputs)]
-accuracy = tf.reduce_mean(correct)
-
-# saving trained model
-save_dir = '../models/rnn'
-saver = tf.train.Saver()
-init = tf.global_variables_initializer()
-
-# pre-process training data
-data = ([], [])
-print('Pre-processing training data...')
-start_time = time.time()
-for i in range(len(transcript) - num_steps - 2):
-    x_text = transcript[i: i + num_steps]
-    y_text = transcript[i + 1: i + num_steps + 1]
-    data[0].append([char2idx[char] for char in x_text])
-    data[1].append([char2idx[char] for char in y_text])
-print('Pre-processed {} characters in {:.2f}s'.format(len(transcript), time.time() - start_time))
-data = (np.array(data[0], dtype=int), np.array(data[1], dtype=int))
-data = np.hstack(data)
-n = data.shape[0]
-data_train, data_validate, data_test = np.split(data, [n - 100, n - 50])
-x_val, y_val = data_validate[:, :num_steps], data_validate[:, num_steps:]
-x_test, y_test = data_test[:, :num_steps], data_test[:, num_steps:]
-
-with tf.Session() as sess:
-
-    print('Starting training...')
-    start_time = time.time()
-    best_accuracy = 0
-    sess.run(init)
-
-    for i in range(max_epochs):
-        np.random.shuffle(data_train)
-        for j in range(data_train.shape[0] // batch_size):
-            start, end = j * batch_size, (j + 1) * batch_size
-            x_batch, y_batch = data_train[start:end, :num_steps], data_train[start:end, num_steps:]
-            sess.run(train_step, feed_dict={x: x_batch, y: y_batch})
-        val_accuracy = sess.run(accuracy, feed_dict={x: x_val, y: y_val})
-        print('Validation accuracy:', val_accuracy)
-        if val_accuracy > best_accuracy:
-            saver.save(sess, save_dir)
-            print('Saving model...')
-        else:
-            saver.restore(sess, save_dir)
-            print('Previous accuracy: {}, restoring model...'.format(best_accuracy))
+if __name__ == '__main__':
+    main()
